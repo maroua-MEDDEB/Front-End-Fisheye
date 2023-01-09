aeed2a34886c1cd1e27d23e1501675b3f6ee1ec9
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-01-09 22:47:53 +0100

    Editt factory pattern pour photographer.js

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    // ajouter city, country
+    const div1 = document.createElement("div");
+    div1.classList.add("location");
+    div1.textContent = city + ", " + country;
+
+    // ajouter tagline
+    const div2 = document.createElement("div");
+    div2.textContent = tagline;
+
+    // ajouter price
+    const div3 = document.createElement("div");
+    div3.textContent = price + "€/jour ";
+    div3.classList.add("price");
+
+    article.appendChild(img);
+    article.appendChild(h2);
+
+    article.appendChild(div1);
+    article.appendChild(div2);
+    article.appendChild(div3);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
