d3988f5524efa322af6226202595ec8caa1766bf
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-02-25 14:09:29 +0100

    finaliser le formulaire de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -15,4 +15,18 @@ contact_btn.addEventListener('click', () => {
   displayModal();
 })
 
+const contact_form = document.querySelector('#contact_form');
 const submit_btn = document.querySelector('#submit_btn');
+
+contact_form.addEventListener('submit', (event) => {
+  event.preventDefault(); // supprimer les actions du bouton de type submit qui sont par défaut
+
+  const prenom  = contact_form.elements['prenom'].value;
+  const nom  = contact_form.elements['nom'].value;
+  const email = contact_form.elements['email'].value;
+  const message = contact_form.elements['message'].value;
+
+  console.log(prenom, nom, email, message);
+  
+  submit_btn.setAttribute('disabled', true); // désactiver le bouton submit après l'envoie de données du formulaire
+});
