8a876d7daed80dde22b4aff4e1d61ffe4efed3ea
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2023-03-02 08:50:28 +0100

    enlever l'attribut disabled du bouton d'enoie de formulaire de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,20 +1,28 @@
+//Afficher le formulaire de contact
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+	  modal.style.display = "block";
 }
 
+//Masquer la formulaire de contact
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
 
-//formulaire de contact
+//Ajouter un évenement de type click au bouton de contact pour afficher le formaulaire de contact
 const contact_btn = document.querySelector('#contact_btn');
-
 contact_btn.addEventListener('click', () => {
   displayModal();
 })
 
+//fermer le modal
+const close_modal = document.querySelector(".close_modal");
+close_modal.addEventListener('click', () => {
+  closeModal();
+});
+
+//ajouter un évênement d'envoie (submit) de formulaire de contact
 const contact_form = document.querySelector('#contact_form');
 const submit_btn = document.querySelector('#submit_btn');
 
@@ -28,11 +36,5 @@ contact_form.addEventListener('submit', (event) => {
 
   console.log(prenom, nom, email, message);
   
-  submit_btn.setAttribute('disabled', true); // désactiver le bouton submit après l'envoie de données du formulaire
-});
-
-//fermer le modal
-const close_modal = document.querySelector(".close_modal");
-close_modal.addEventListener('click', () => {
-  closeModal()
+  //submit_btn.setAttribute('disabled', true); // désactiver le bouton submit après l'envoie de données du formulaire (on a le droit d'envoyer  les données  q'une seule fois)
 });
